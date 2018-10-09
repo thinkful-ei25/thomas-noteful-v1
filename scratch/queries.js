8e59cde17b37f47e4521fe6a53d6082ed261d609
Thomas Chun <tchun2055@gmail.com>
2018-10-09 13:15:24 -0700

    add simple in memory database

---
+++ scratch/queries.js
@@ -0,0 +1,79 @@
+'use strict';
+
+const data = require('../db/notes');
+const simDB = require('../db/simDB');
+const notes = simDB.initialize(data);
+
+// GET Notes with search
+notes.filter('cats', (err, list) => {
+  if (err) {
+    console.error(err);
+  }
+  console.log(list);
+});
+
+// GET Notes by ID
+notes.find(1005, (err, item) => {
+  if (err) {
+    console.error(err);
+  }
+  if (item) {
+    console.log(item);
+  } else {
+    console.log('not found');
+  }
+});
+
+// PUT (Update) Notes by ID
+const updateObj = {
+  title: 'New Title',
+  content: 'Blah blah blah'
+};
+
+notes.update(1005, updateObj, (err, item) => {
+  if (err) {
+    console.error(err);
+  }
+  if (item) {
+    console.log(item);
+  } else {
+    console.log('not found');
+  }
+});
+
+const newItem = {
+  'id': 0,
+  'title': 'TEST TITLE USING THE NOTES.CREATE METHOD',
+  'content': 'THIS IS THE NEW CONTENT BEING ADDED AS A TEST'
+};
+
+notes.create(newItem, (err, item) => {
+  if (err) {
+    console.error(err);
+  }
+  if (item) {
+    console.log(item);
+  }
+});
+
+notes.delete(1003, (err, item) => {
+  if (err) {
+    console.error(err);
+  }
+  if (item) {
+    console.log(item);
+  } else {
+    console.log('not found');
+  }
+}); 
+
+notes.find(1003, (err, item) => {
+  if (err) {
+    console.error(err);
+  }
+  if (item) {
+    console.log(item);
+  } else {
+    console.log('not found');
+  }
+});
