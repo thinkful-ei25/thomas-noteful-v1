1c7df27a9dc1f60bedc3606be39655c9ab1887e3
Thomas Chun <tchun2055@gmail.com>
2018-10-10 12:34:14 -0700

    added create new POST endpoint

--- router/notes.router.js
+++ router/notes.router.js
@@ -57,6 +57,31 @@ notesRouter.put('/notes/:id', (req, res, next) => {
   });
 });
 
+// Post (insert) an item
+notesRouter.post('/notes', (req, res, next) => {
+  const { title, content } = req.body;
+
+  const newItem = { title, content };
+  /***** Never trust users - validate input *****/
+  if (!newItem.title) {
+    const err = new Error('Missing `title` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  notes.create(newItem, (err, item) => {
+    if (err) {
+      return next(err);
+    }
+    if (item) {
+      res.location(`http://${req.headers.host}/notes/${item.id}`).status(201).json(item);
+    } else {
+      next();
+    }
+  });
+});
+
+
 notesRouter.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
