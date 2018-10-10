286b971702b85d148ac8afd2ccff845fd0769cdd
Thomas Chun <tchun2055@gmail.com>
2018-10-10 12:29:01 -0700

    moved endpoints to express router

---
+++ router/notes.router.js
@@ -0,0 +1,74 @@
+'use strict';
+
+const express = require('express');
+const notesRouter = express.Router();
+const jsonParser = express.json();
+
+const data = require('../db/notes');
+const simDB = require('../db/simDB');
+const notes = simDB.initialize(data);
+
+
+notesRouter.get('/notes/', (req, res, next) => {
+  const { searchTerm } = req.query;
+
+  notes.filter(searchTerm, (err, list) => {
+    if (err) {
+      return next(err); // goes to error handler
+    }
+    res.json(list); // responds with filtered array
+  });
+});
+
+notesRouter.get('/notes/:id', (req, res, next) => {
+  const id = req.params.id;
+  notes.find(id, (err, item) => {
+    if (err) {
+      return next(err); // goes to error handler
+    }
+    res.json(item);
+  });
+});
+
+notesRouter.put('/notes/:id', (req, res, next) => {
+  const id = req.params.id;
+
+  /***** Never trust users - validate input *****/
+  const updateObj = {};
+  const updateFields = ['title', 'content'];
+
+  updateFields.forEach(field => {
+    if (field in req.body) {
+      updateObj[field] = req.body[field];
+    }
+  });
+  // console.log(req.body);
+  // console.log(updateObj);
+
+  notesRouter.update(id, updateObj, (err, item) => {
+    if (err) {
+      return next(err);
+    }
+    if (item) {
+      res.json(item);
+    } else {
+      next();
+    }
+  });
+});
+
+notesRouter.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  res.status(404).json({ message: 'Not Found' });
+});
+
+notesRouter.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
+});
+
+module.exports = notesRouter;
--- server.js
+++ server.js
@@ -1,33 +1,41 @@
 'use strict';
 
 const express = require('express');
-const data = require('./db/notes');
-const simDB = require('./db/simDB');
 const morgan = require('morgan');
-const notes = simDB.initialize(data);
-
+// const data = require('./db/notes');
+// const simDB = require('./db/simDB');
+// const notes = simDB.initialize(data);
 const app = express();
 const { PORT } = require('./config');
 // const { logger } = require('./middleware/logger');
 // app.use(logger);
+const notesRouter = require('./router/notes.router');
 
 app.use(morgan('common'));
-
 app.use(express.static('public'));
-
 app.use(express.json());
+app.use('/api', notesRouter);
 
-app.get('/api/notes', (req, res, next) => {
-  const { searchTerm } = req.query;
+// ADD STATIC SERVER HERE
+app.listen(PORT, function () {
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+}); 
 
-  notes.filter(searchTerm, (err, list) => {
-    if (err) {
-      return next(err); // goes to error handler
-    }
-    res.json(list); // responds with filtered array
-  });
-});
 
+// app.get('/api/notes', (req, res, next) => {
+//   const { searchTerm } = req.query;
+
+//   notes.filter(searchTerm, (err, list) => {
+//     if (err) {
+//       return next(err); // goes to error handler
+//     }
+//     res.json(list); // responds with filtered array
+//   });
+// });
+
+// *** Same code as above ^
 // const searchTerm = req.query.searchTerm;
 // let searchToLowerCase = '';
 
@@ -47,72 +55,54 @@ app.get('/api/notes', (req, res, next) => {
 // }
 // });
 
-app.get('/api/notes/:id', (req, res, next) => {
-  const id = req.params.id;
-  notes.find(id, (err, item) => {
-    if (err) {
-      return next(err); // goes to error handler
-    }
-    res.json(item);
-  });
-});
-// app.get('/api/notes/:id', (req, res) => {
+// app.get('/api/notes/:id', (req, res, next) => {
 //   const id = req.params.id;
-//   res.json(data.find(item => item.id === Number(id)));
+//   notes.find(id, (err, item) => {
+//     if (err) {
+//       return next(err); // goes to error handler
+//     }
+//     res.json(item);
+//   });
 // });
 
-// test error
-// app.get('/boom', (req, res, next) => {
-//   throw new Error('Boom!!');
+// app.put('/api/notes/:id', (req, res, next) => {
+//   const id = req.params.id;
+
+//   /***** Never trust users - validate input *****/
+//   const updateObj = {};
+//   const updateFields = ['title', 'content'];
+
+//   updateFields.forEach(field => {
+//     if (field in req.body) {
+//       updateObj[field] = req.body[field];
+//     }
+//   });
+//   console.log(req.body);
+//   console.log(updateObj);
+
+//   notes.update(id, updateObj, (err, item) => {
+//     if (err) {
+//       return next(err);
+//     }
+//     if (item) {
+//       res.json(item);
+//     } else {
+//       next();
+//     }
+//   });
 // });
 
 
-app.put('/api/notes/:id', (req, res, next) => {
-  const id = req.params.id;
+// app.use(function (req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   res.status(404).json({ message: 'Not Found' });
+// });
 
-  /***** Never trust users - validate input *****/
-  const updateObj = {};
-  const updateFields = ['title', 'content'];
-
-  updateFields.forEach(field => {
-    if (field in req.body) {
-      updateObj[field] = req.body[field];
-    }
-  });
-  console.log(req.body);
-  console.log(updateObj);
-
-  notes.update(id, updateObj, (err, item) => {
-    if (err) {
-      return next(err);
-    }
-    if (item) {
-      res.json(item);
-    } else {
-      next();
-    }
-  });
-});
-
-
-app.use(function (req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  res.status(404).json({ message: 'Not Found' });
-});
-
-app.use(function (err, req, res, next) {
-  res.status(err.status || 500);
-  res.json({
-    message: err.message,
-    error: err
-  });
-});
-
-// ADD STATIC SERVER HERE
-
-app.listen(PORT, function () {
-  console.info(`Server listening on ${this.address().port}`);
-}).on('error', err => {
-  console.error(err);
-}); 
+// app.use(function (err, req, res, next) {
+//   res.status(err.status || 500);
+//   res.json({
+//     message: err.message,
+//     error: err
+//   });
+// });
